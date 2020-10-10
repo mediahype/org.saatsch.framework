fa39bcfb35913e94c246f1b5d92ec247ace8c519
saatsch <saatsch@gmail.com>
2020-10-10 18:16:52 +0200

    prevented a test from creating persistent local files

--- base/serializing/src/test/java/org/saatsch/framework/base/serializing/SerializingTest.java
+++ base/serializing/src/test/java/org/saatsch/framework/base/serializing/SerializingTest.java
@@ -3,6 +3,7 @@ package org.saatsch.framework.base.serializing;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,15 +13,14 @@ import org.junit.rules.TemporaryFolder;
 public class SerializingTest {
 
   @Rule
-  public TemporaryFolder folder= new TemporaryFolder();
+  public TemporaryFolder folder = new TemporaryFolder();
   
   private ObjectForSerializer obj = new ObjectForSerializer();
   
   @Test
-  public void testJson() throws SerializerException {
+  public void testJson() throws SerializerException, IOException {
     
-    // File createdFile = folder.newFile("myJson.json");
-    File createdFile = new File("myJson.json");
+    File createdFile =  folder.newFile("myJson.json"); 
     JsonSerializer serializer = SerializerFactory.newJsonSerializer();
     serializer.save(createdFile, obj);
     assertNotNull(serializer.objectToString(obj));
@@ -28,8 +28,9 @@ public class SerializingTest {
   }
 
   @Test
-  public void testXml() throws SerializerException {
-    File createdFile = new File("myXml.xml");
+  public void testXml() throws SerializerException, IOException {
+    
+    File createdFile = folder.newFile("myXml.xml");
     XmlSerializer serializer = SerializerFactory.newXmlSerializer();
     serializer.save(createdFile, obj);
     serializer.load(ObjectForSerializer.class, createdFile);
